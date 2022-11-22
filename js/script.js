4dffb80e4bcc84b6cab6382e500c7f57af6ba76c
francescotrudu <francescotrudu84@gmail.com>
2022-11-22 11:11:15 +0100

    refactoring js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ while (age !== parseInt(age.toString(1)).toString()) {
 }
 
 const kmPrice = 0.21;
-const totPrice = kmPrice * km;
+let totPrice = kmPrice * km;
 const discountUnder = (20 / 100);
 const discountOver_40 = (40 / 100);
 const under = ((totPrice) - (totPrice * discountUnder));
@@ -24,12 +24,12 @@ const over = ((totPrice) - (totPrice * discountOver_40));
 
 if ((age >=0) && (age <18)) {
     // console.log((totPrice) - (totPrice * discountUnder));
-    document.getElementById("total").innerHTML = ("&euro;" + under.toFixed(2));
+    totPrice = (under.toFixed(2));
 } else if (age > 65) {
     // console.log((totPrice) - (totPrice * discoutOver_40));
-    document.getElementById("total").innerHTML = ("&euro;" + over.toFixed(2));
+    totPrice = (over.toFixed(2));
+} 
 
-} else {
-    document.getElementById("total").innerHTML = ("&euro;" + totPrice.toFixed(2));
-}
+document.getElementById("total").innerHTML = ("&euro;" + totPrice);
+
 // console.log(totPrice);
